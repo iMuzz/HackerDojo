8716eff51e70a78a834a4871a3419e393439bb61
Michael Aubry <michaelaubry@Michaels-MacBook-Pro-2.local>
2015-11-07 15:52:30 -0800

    fixed the component structure and markup

---
+++ client/components/trips/trips.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('emiratesProjectApp')
+  .controller('TripsCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });

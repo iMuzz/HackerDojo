34b9b9629747d88ffe35e4e45a46675e605dce75
Faraaz Nishtar <faraaznishtar@Faraazs-MacBook-Pro.local>
2015-11-07 21:02:30 -0800

    trip details showing up in modal

--- client/components/services/tripDataFactory.js
+++ client/components/services/tripDataFactory.js
@@ -18,6 +18,125 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 				returnArrivalDate: ""
 			}
 		},
+		{
+			location: "Bahamas",
+			description: "A beautiful getaway in the exotic country of Peru",
+			totalPrice: "2000",
+			imageUrl: "http://cdni.condenast.co.uk/646x430/o_r/peru_cnt_9nov09_istock_b.jpg",
+			lodge: {
+				imageUrl: "",
+				description: "",
+				price: ""
+			},
+			flight: {
+				departureDate: "",
+				arrivalDate: "",
+				returnDepartureDate: "",
+				returnArrivalDate: ""
+			}
+		},
+		{
+			location: "Peru",
+			description: "A beautiful getaway in the exotic country of Peru",
+			totalPrice: "2000",
+			imageUrl: "http://cdni.condenast.co.uk/646x430/o_r/peru_cnt_9nov09_istock_b.jpg",
+			lodge: {
+				imageUrl: "",
+				description: "",
+				price: ""
+			},
+			flight: {
+				departureDate: "",
+				arrivalDate: "",
+				returnDepartureDate: "",
+				returnArrivalDate: ""
+			}
+		},
+		{
+			location: "Peru",
+			description: "A beautiful getaway in the exotic country of Peru",
+			totalPrice: "2000",
+			imageUrl: "http://cdni.condenast.co.uk/646x430/o_r/peru_cnt_9nov09_istock_b.jpg",
+			lodge: {
+				imageUrl: "",
+				description: "",
+				price: ""
+			},
+			flight: {
+				departureDate: "",
+				arrivalDate: "",
+				returnDepartureDate: "",
+				returnArrivalDate: ""
+			}
+		},
+		{
+			location: "Peru",
+			description: "A beautiful getaway in the exotic country of Peru",
+			totalPrice: "2000",
+			imageUrl: "http://cdni.condenast.co.uk/646x430/o_r/peru_cnt_9nov09_istock_b.jpg",
+			lodge: {
+				imageUrl: "",
+				description: "",
+				price: ""
+			},
+			flight: {
+				departureDate: "",
+				arrivalDate: "",
+				returnDepartureDate: "",
+				returnArrivalDate: ""
+			}
+		},
+		{
+			location: "Peru",
+			description: "A beautiful getaway in the exotic country of Peru",
+			totalPrice: "2000",
+			imageUrl: "http://cdni.condenast.co.uk/646x430/o_r/peru_cnt_9nov09_istock_b.jpg",
+			lodge: {
+				imageUrl: "",
+				description: "",
+				price: ""
+			},
+			flight: {
+				departureDate: "",
+				arrivalDate: "",
+				returnDepartureDate: "",
+				returnArrivalDate: ""
+			}
+		},
+		{
+			location: "Peru",
+			description: "A beautiful getaway in the exotic country of Peru",
+			totalPrice: "2000",
+			imageUrl: "http://cdni.condenast.co.uk/646x430/o_r/peru_cnt_9nov09_istock_b.jpg",
+			lodge: {
+				imageUrl: "",
+				description: "",
+				price: ""
+			},
+			flight: {
+				departureDate: "",
+				arrivalDate: "",
+				returnDepartureDate: "",
+				returnArrivalDate: ""
+			}
+		},
+		{
+			location: "Peru",
+			description: "A beautiful getaway in the exotic country of Peru",
+			totalPrice: "2000",
+			imageUrl: "http://cdni.condenast.co.uk/646x430/o_r/peru_cnt_9nov09_istock_b.jpg",
+			lodge: {
+				imageUrl: "",
+				description: "",
+				price: ""
+			},
+			flight: {
+				departureDate: "",
+				arrivalDate: "",
+				returnDepartureDate: "",
+				returnArrivalDate: ""
+			}
+		},
 	]
 
 	return trips;
--- client/components/trips/trips.controller.js
+++ client/components/trips/trips.controller.js
@@ -4,7 +4,9 @@ angular.module('emiratesProjectApp')
   .controller('TripsCtrl', function ($scope, $location, tripDataFactory) {
 
     $scope.trips = tripDataFactory;
-    
+    $scope.chosenTrip = {};
+    $scope.detailsIsShown = { val: false};
+
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
@@ -16,7 +18,14 @@ angular.module('emiratesProjectApp')
       return route === $location.path();
     };
 
-    $scope.showDetails = function(){
-      
+    $scope.showTripDetail = function(trip){
+      console.log("Trip chosen: ", trip);
+      $scope.detailsIsShown.val = true;
+      $scope.chosenTrip = trip;
+    }
+
+    $scope.close = function(){
+        $scope.detailsIsShown.val = false;
+        console.log("This should be closed!");
     }
   });

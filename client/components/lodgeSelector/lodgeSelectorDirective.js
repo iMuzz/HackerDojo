f7d8bc48933e00ef43b40db312da78eba21f1989
Faraaz Nishtar <faraaznishtar@Faraazs-MacBook-Pro.local>
2015-11-08 05:51:41 -0800

    initalize lodge Detail with first lodge

--- client/components/lodgeSelector/lodgeSelectorDirective.js
+++ client/components/lodgeSelector/lodgeSelectorDirective.js
@@ -3,16 +3,24 @@ angular.module('emiratesProjectApp').directive('lodgeSelector', function(){
 		restrict: 'E',
 		templateUrl: "components/lodgeSelector/lodgeSelector.html",
 		scope: {
-			lodges: "="
+			lodges: "=",
 		},
 		link: function(scope, el, attrs) {
-			// 
+			
 			scope.selectedLodge = {};
 			
 			scope.viewLodge = function(lodge){
 				console.log("viewLodge called: ", lodge);
 				scope.selectedLodge = lodge;
 			};
+
+			var clear = scope.$watchCollection('lodges', function(newVals, oldVals){
+				console.log("Ran!");
+				if (newVals) {
+					scope.selectedLodge = newVals[0];
+					clear();
+				};
+			});
 		}
 	}
 });

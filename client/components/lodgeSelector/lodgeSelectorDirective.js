7190a8664755f092181904283767f8db26f21aa7
Faraaz Nishtar <faraaznishtar@Faraazs-MacBook-Pro.local>
2015-11-08 08:47:36 -0800

    active state working

--- client/components/lodgeSelector/lodgeSelectorDirective.js
+++ client/components/lodgeSelector/lodgeSelectorDirective.js
@@ -7,13 +7,23 @@ angular.module('emiratesProjectApp').directive('lodgeSelector', function(){
 		},
 		link: function(scope, el, attrs) {
 			scope.selectedLodge = {};
+
+			var old_active;
+
 			scope.viewLodge = function(lodge){
 				console.log("viewLodge called: ", lodge);
 				scope.selectedLodge = lodge;
+				
+				if(old_active != undefined) {
+					$(old_active).removeClass("active");
+				}
+
+				old_active = event.currentTarget;
+				$(event.currentTarget).addClass("active");
 			};
 
+
 			var clear = scope.$watchCollection('lodges', function(newVals, oldVals){
-				console.log("Ran!");
 				if (newVals) {
 					scope.selectedLodge = newVals[0];
 					clear();

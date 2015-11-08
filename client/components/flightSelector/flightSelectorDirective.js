45242fbb8526df7bb4528b8593eaf3b3080b8319
Michael Aubry <michaelaubry@Michaels-MacBook-Pro-2.local>
2015-11-08 10:30:04 -0800

    sj

--- client/components/flightSelector/flightSelectorDirective.js
+++ client/components/flightSelector/flightSelectorDirective.js
@@ -8,10 +8,18 @@ angular.module('emiratesProjectApp').directive('flightSelector', function(){
 		link: function(scope, el, attrs){
 
 			scope.selectedRoundtripFlight = {};
+			var old_active;
 
 			scope.viewFlight = function(roundtripFlight){
 				console.log("viewFlight called: ", roundtripFlight);
 				//scope.selectedRoundtripFlight = roundtripFlight;
+
+				if(old_active != undefined) {
+					$(old_active).removeClass("active");
+				}
+
+				old_active = event.currentTarget;
+				$(event.currentTarget).addClass("active");
 			};
 
 			var clear = scope.$watchCollection('roundtripFlights', function(newVals, oldVals){

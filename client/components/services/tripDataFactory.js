1ecd820b1db9463ded87869630af7cf9524b2a93
Michael Aubry <michaelaubry@Michaels-MacBook-Pro-2.local>
2015-11-08 08:01:14 -0800

    did stuff

--- client/components/services/tripDataFactory.js
+++ client/components/services/tripDataFactory.js
@@ -13,6 +13,7 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 					imageUrl: "http://media-cdn.tripadvisor.com/media/photo-o/08/27/95/9a/belmond-sanctuary-lodge.jpg",
 					description: "Blemond Sanctuary Lodge",
 					price: "500",
+					about: "Cozy up in our recently renovated cabin. 2 bedrooms, 2 baths and a private yard with fire pit and hot tub! Heavenly Village is a 5-minute drive. Casinos not much farther.",
 					num_guests: 2
 				},
 				{
@@ -21,6 +22,7 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 					imageUrl: "http://media-cdn.tripadvisor.com/media/photo-o/08/27/95/9a/belmond-sanctuary-lodge.jpg",
 					description: "Blemond Sanctuary Lodge",
 					price: "500",
+					about: "Cozy up in our recently renovated cabin. 2 bedrooms, 2 baths and a private yard with fire pit and hot tub! Heavenly Village is a 5-minute drive. Casinos not much farther.",
 					num_guests:  2
 				},
 				{
@@ -29,6 +31,7 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 					imageUrl: "http://media-cdn.tripadvisor.com/media/photo-o/08/27/95/9a/belmond-sanctuary-lodge.jpg",
 					description: "Blemond Sanctuary Lodge",
 					price: "500",
+					about: "Cozy up in our recently renovated cabin. 2 bedrooms, 2 baths and a private yard with fire pit and hot tub! Heavenly Village is a 5-minute drive. Casinos not much farther.",
 					num_guests: 2
 				},
 				{
@@ -37,6 +40,7 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 					imageUrl: "http://media-cdn.tripadvisor.com/media/photo-o/08/27/95/9a/belmond-sanctuary-lodge.jpg",
 					description: "Blemond Sanctuary Lodge",
 					price: "500",
+					about: "Cozy up in our recently renovated cabin. 2 bedrooms, 2 baths and a private yard with fire pit and hot tub! Heavenly Village is a 5-minute drive. Casinos not much farther.",
 					num_guests: 2
 				},
 				{
@@ -45,6 +49,7 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 					imageUrl: "http://media-cdn.tripadvisor.com/media/photo-o/08/27/95/9a/belmond-sanctuary-lodge.jpg",
 					description: "Blemond Sanctuary Lodge",
 					price: "500",
+					about: "Cozy up in our recently renovated cabin. 2 bedrooms, 2 baths and a private yard with fire pit and hot tub! Heavenly Village is a 5-minute drive. Casinos not much farther.",
 					num_guests: 2
 				},
 			],
@@ -64,6 +69,7 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 				{
 					imageUrl: "http://cdni.condenast.co.uk/646x430/a_c/ayada_cnt_25aug11_pr.jpg",
 					description: "Maldives Beach Lodge",
+					about: "Cozy up in our recently renovated cabin. 2 bedrooms, 2 baths and a private yard with fire pit and hot tub! Heavenly Village is a 5-minute drive. Casinos not much farther.",
 					price: "300"
 				}
 			],
@@ -83,6 +89,7 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 				{
 					imageUrl: "http://www.famousnychotels.com/bpcms/ImageUpload/hotelphotos/warwickny.jpg",
 					description: "Warwick Hotel",
+					about: "Cozy up in our recently renovated cabin. 2 bedrooms, 2 baths and a private yard with fire pit and hot tub! Heavenly Village is a 5-minute drive. Casinos not much farther.",
 					price: "1000"
 				}
 			],
@@ -160,6 +167,82 @@ angular.module('emiratesProjectApp').factory('tripDataFactory', function(){
 					imageUrl: "http://insidekyoto.smugmug.com/Kyoto-Hotels/The-Gion-House/i-WXBXWht/0/M/gion-house-3-M.jpg",
 					description: "Ryokan Inn",
 					price: "200"
+				}
+			],
+			flight: {
+				departureDate: new Date(),
+				arrivalDate: new Date(),
+				returnDepartureDate: new Date().setDate(new Date().getDate() + 7),
+				returnArrivalDate: new Date().setDate(new Date().getDate() + 7)
+			}
+		},
+		{
+			location: "Los Angeles",
+			description: "Party with the stars in the City of Angels",
+			totalPrice: "1000",
+			imageUrl: "./assets/images/dubai.jpg",
+			lodges: [
+				{
+					imageUrl: "http://assets.sheratonlax.com/XLGallery/md/she344ex22426.jpg",
+					description: "Sheraton Los Angeles",
+					price: "400"
+				}
+			],
+			flight: {
+				departureDate: new Date(),
+				arrivalDate: new Date(),
+				returnDepartureDate: new Date().setDate(new Date().getDate() + 7),
+				returnArrivalDate: new Date().setDate(new Date().getDate() + 7)
+			}
+		},
+		{
+			location: "Los Angeles",
+			description: "Party with the stars in the City of Angels",
+			totalPrice: "1000",
+			imageUrl: "./assets/images/dubai.jpg",
+			lodges: [
+				{
+					imageUrl: "http://assets.sheratonlax.com/XLGallery/md/she344ex22426.jpg",
+					description: "Sheraton Los Angeles",
+					price: "400"
+				}
+			],
+			flight: {
+				departureDate: new Date(),
+				arrivalDate: new Date(),
+				returnDepartureDate: new Date().setDate(new Date().getDate() + 7),
+				returnArrivalDate: new Date().setDate(new Date().getDate() + 7)
+			}
+		},
+		{
+			location: "Los Angeles",
+			description: "Party with the stars in the City of Angels",
+			totalPrice: "1000",
+			imageUrl: "./assets/images/dubai.jpg",
+			lodges: [
+				{
+					imageUrl: "http://assets.sheratonlax.com/XLGallery/md/she344ex22426.jpg",
+					description: "Sheraton Los Angeles",
+					price: "400"
+				}
+			],
+			flight: {
+				departureDate: new Date(),
+				arrivalDate: new Date(),
+				returnDepartureDate: new Date().setDate(new Date().getDate() + 7),
+				returnArrivalDate: new Date().setDate(new Date().getDate() + 7)
+			}
+		},
+		{
+			location: "Los Angeles",
+			description: "Party with the stars in the City of Angels",
+			totalPrice: "1000",
+			imageUrl: "./assets/images/dubai.jpg",
+			lodges: [
+				{
+					imageUrl: "http://assets.sheratonlax.com/XLGallery/md/she344ex22426.jpg",
+					description: "Sheraton Los Angeles",
+					price: "400"
 				}
 			],
 			flight: {
